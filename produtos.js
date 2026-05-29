71ea44c16498fe8d178d8c51d823a61553de2ee1
Pedro Henrique Guimarães <202507077491@alunos.estacio.br>
2026-05-29 15:55:19 -0300

    Removidos 2 produtos do catálogo Box Parfum

---
+++ produtos.js
@@ -0,0 +1,10 @@
+const productsData = [
+  {
+    name: "Nome do Produto",
+    category: "Perfume",
+    price: 200,
+    stock: 1,
+    image: "imagem.png",
+    desc: "Descrição curta do produto."
+  }
+];
